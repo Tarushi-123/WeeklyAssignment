ce25d8d3e98fad1d8dbc543ddbcd0b31cc521dae
Tarushi-123 <tarushitainwala@gmail.com>
2026-02-17 11:05:02 +0530

    question 2 completed

---
+++ SamAverage.java
@@ -0,0 +1,14 @@
+class SamAverage {
+    public static void main(String[] args) {
+
+        // Marks
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        // Average calculation
+        double average = (maths + physics + chemistry) / 3.0;
+
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
